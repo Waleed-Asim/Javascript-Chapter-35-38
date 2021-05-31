b60b9fd4180992fc501d1e268096e90803b64b10
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-01 02:31:53 +0500

    Add initial files

---
+++ app.js
@@ -0,0 +1,35 @@
+// Q1
+
+// function date(){
+//     var currentDate = new Date();
+//     console.log(currentDate);
+// }
+
+// date();
+
+// Q2
+
+// function user(){
+//     var firstName = prompt("Write first Name");
+//     var lastName = prompt("Write last Name");
+//     alert("Hello! " + firstName + " " + lastName);
+// }
+
+// user();
+
+// Q3
+
+// function calc() {
+//     var a = +prompt("Write a number");
+//     var b = +prompt("Write another number");
+//     return a + b;
+// }
+// console.log(calc());
+
+// Q5
+
+// function calc(a,b) {
+//     return (a + b)*2;
+// }
+// console.log(calc(5,2));
+
